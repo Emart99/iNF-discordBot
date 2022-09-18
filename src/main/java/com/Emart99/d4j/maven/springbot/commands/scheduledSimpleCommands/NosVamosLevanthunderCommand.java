e141b0b883378c7a2f821878035caf8dce86b39d
Ezequiel Martino <ezequielmartino@MacBook-Pro-de-Ezequiel.local>
2022-09-18 19:54:45 -0300

    remake scheduleCommand

--- src/main/java/com/Emart99/d4j/maven/springbot/commands/scheduledSimpleCommands/NosVamosLevanthunderCommand.java
+++ src/main/java/com/Emart99/d4j/maven/springbot/commands/scheduledSimpleCommands/NosVamosLevanthunderCommand.java
@@ -6,30 +6,38 @@ import discord4j.core.object.entity.channel.MessageChannel;
 import discord4j.core.object.entity.channel.TextChannel;
 import discord4j.core.object.reaction.ReactionEmoji;
 
+import java.time.Duration;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Calendar;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class NosVamosLevanthunderCommand {
+
     public void execute(GatewayDiscordClient client){
-        Timer timer = new Timer();
-        Calendar date = Calendar.getInstance();
-        date.set(Calendar.HOUR_OF_DAY,11);
-        date.set(Calendar.MINUTE,0);
-        date.set(Calendar.SECOND,0);
-        date.set(Calendar.MILLISECOND,0);
         MessageChannel textChannel = client.getChannelById(Snowflake.of(442868635711832068L)).cast(TextChannel.class).block();
         Long twitIconId = 689959593698394198L;
         ReactionEmoji twitIconReaction = ReactionEmoji.of(twitIconId,"twitchicono",false);
-        timer.schedule(
-                new TimerTask() {
+
+        ZonedDateTime buenosAiresTime =  ZonedDateTime.now(ZoneId.of("America/Buenos_Aires"));
+        ZonedDateTime nextRun = buenosAiresTime.withHour(8).withMinute(0).withSecond(0);
+        if(buenosAiresTime.compareTo(nextRun) > 0){
+            nextRun = nextRun.plusDays(1);
+        }
+        Duration duration = Duration.between(buenosAiresTime,nextRun);
+        Long initialDelay = duration.getSeconds();
+
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(
+                new Runnable() {
                     @Override
                     public void run() {
                         textChannel.createMessage("nos vamo levanthunder <:twitchicono:689959593698394198>").block().addReaction(twitIconReaction).block();
                     }
-                },
-                date.getTime(),
-                1000 * 60 * 60 * 24
-        );
+                }, initialDelay, TimeUnit.DAYS.toSeconds(1), TimeUnit.SECONDS);
     }
 }
