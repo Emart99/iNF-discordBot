b79834397fe874617036f33f346e877024c4f776
Emart99 <ezequielsimon22@yahoo.com.ar>
2023-04-11 17:57:16 -0300

    ping fixes

--- src/main/java/com/github/ezeakel/infBot/Commands/SimpleTextCommands/PingCommand.java
+++ src/main/java/com/github/ezeakel/infBot/Commands/SimpleTextCommands/PingCommand.java
@@ -3,20 +3,23 @@ package com.github.ezeakel.infBot.Commands.SimpleTextCommands;
 import com.github.ezeakel.infBot.Commands.Command;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.MessageChannel;
+import discord4j.rest.util.Color;
 import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.time.Instant;
+import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
 
 public class PingCommand implements Command {
     @Override
-    public Mono<Void> execute(MessageCreateEvent event) throws IOException, URISyntaxException, InterruptedException {
-        String ping = (Instant.now().minus(event.getMessage().getTimestamp().toEpochMilli(), ChronoUnit.MILLIS).toString())+"ms";
+    public Mono<Void> execute(MessageCreateEvent event) {
         return Mono.justOrEmpty(event.getMessage())
                 .flatMap(Message::getChannel)
-                .flatMap(messageChannel -> messageChannel.createMessage(ping))
+                .flatMap(messageChannel ->  messageChannel.createMessage("Pong: "+ (System.currentTimeMillis() - event.getMessage().getTimestamp().toEpochMilli() ) + "ms"))
                 .then();
     }
 }
+
